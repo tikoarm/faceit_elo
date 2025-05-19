aa1216409fd42713284c8b85d045ea50221406c2
bauld227 <96850805+bauld227@users.noreply.github.com>
2025-05-19 16:22:34 +0200

    2

--- old/psf.limited1/Bonnaa/config.php
+++
@@ -1,4 +0,0 @@
-<?
-define('USER_TOKEN', 'Ck729S4ycyFDe6F7');
-define('ADMIN_TOKEN', 'R4477HkdkFDK9s7v');
-define('DIR', '/Bonnaa');
--- old/psf.limited1/tocopy/config.php
+++
@@ -1,4 +0,0 @@
-<?
-define('USER_TOKEN', 'jif7P82K3pF54JuE');
-define('ADMIN_TOKEN', 'F93f65UdVi2v3NmG');
-define('DIR', '/kyson');
