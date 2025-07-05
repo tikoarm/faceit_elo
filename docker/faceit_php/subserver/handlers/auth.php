f96ad4357d9625cc3e20ce8e30885f1957ae16d9
tikoarm <209512015+tikoarm@users.noreply.github.com>
2025-07-05 17:41:30 +0200

    Add env sample and use env var for subserver password
    Introduced docker/env_sample.txt to provide environment variable templates for configuration. Updated auth.php to retrieve the subserver password from the SUBSERVERS_PASSWORD environment variable, improving security and configurability.

--- docker/faceit_php/subserver/handlers/auth.php
+++ docker/faceit_php/subserver/handlers/auth.php
@@ -1,6 +1,6 @@
 <?php
 
-$correctPassword = '123';
+$correctPassword = getenv('SUBSERVERS_PASSWORD') ?: 'mD9!sL#8f@VqZ2^gR3w*Bt7$NyX&6UcK';
 $password = $_POST['password'] ?? '';
 if ($password === $correctPassword) {
     $_SESSION['authenticated'] = true;
